382e8fe7e328638326678caaf6df957c9b1a3b72
STHyeon <toby2009@naver.com>
2020-01-20 15:03:45 +0900

    init

---
+++ react_ver/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <header className="App-header">pa</header>
+        </div>
+    );
+};
+
+export default App;
