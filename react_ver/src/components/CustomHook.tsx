693c9ae9d7a68eee1141b500ac61ee55e82a3fb2
STHyeon <toby2009@naver.com>
2020-01-22 11:39:35 +0900

    CLOCK

---
+++ react_ver/src/components/CustomHook.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+export const useTimer = () => {
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+
+    useEffect(() => {
+        let interval: any;
+        if (isRunning) {
+            interval = setInterval(() => setElapsedTime(prevElapsedTime => prevElapsedTime + 0.1), 100);
+        }
+        return () => clearInterval(interval);
+    }, [isRunning]);
+
+    return {
+        isRunning,
+        setIsRunning,
+        elapsedTime,
+        setElapsedTime
+    };
+};
+
+export const useStopwatch = () => {
+    const [laps, setLaps] = useState<any>([]);
+    const { isRunning, setIsRunning, elapsedTime, setElapsedTime } = useTimer();
+
+    const handleReset = () => {
+        setIsRunning(false);
+        setElapsedTime(0);
+        setLaps([]);
+    };
+
+    const handleAddLap = () => {
+        const prevTotal: any = laps.length > 0 ? laps.reduce((acc: any, curr: any) => acc + curr, 0) : 0;
+        const currentLap: any = elapsedTime;
+        isRunning && setLaps([...laps, currentLap]);
+    };
+
+    return {
+        elapsedTime: elapsedTime.toFixed(1),
+        laps,
+        addLap: () => handleAddLap(),
+        resetTimer: () => handleReset(),
+        startTimer: () => setIsRunning(true),
+        stopTimer: () => setIsRunning(false),
+        isRunning
+    };
+};
---
+++ react_ver/src/components/Laps.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Laps = (lap: any) => <li>{lap}</li>;
+
+export default Laps;
