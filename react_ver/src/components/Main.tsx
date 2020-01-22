53d6d86109c799b37c03c36fccf3a158afa8a222
STHyeon <toby2009@naver.com>
2020-01-22 17:43:48 +0900

    main

--- react_ver/src/components/Main.tsx
+++ react_ver/src/components/Main.tsx
@@ -1,9 +1,67 @@
 import React from "react";
+import MAIN_IMG from "../lib/img/icon/main.jpg";
 
 function Main() {
     return (
         <main>
-            <p>ab</p>
+            <div className="inner">
+                <div className="mainBox">
+                    <div className="card">
+                        김관자
+                        <h1>김첨지가 왜 그랬을까요?</h1>
+                        <img src={MAIN_IMG} alt="" />
+                    </div>
+                </div>
+                <div className="mainBox">
+                    <div className="card">
+                        김관자
+                        <h1>김첨지가 왜 그랬을까요?</h1>
+                        <img src={MAIN_IMG} alt="" />
+                    </div>
+                </div>
+                <div className="mainBox">
+                    <div className="card">
+                        김관자
+                        <h1>김첨지가 왜 그랬을까요?</h1>
+                        <img src={MAIN_IMG} alt="" />
+                    </div>
+                </div>
+                <div className="mainBox">
+                    <div className="card">
+                        김관자
+                        <h1>김첨지가 왜 그랬을까요?</h1>
+                        <img src={MAIN_IMG} alt="" />
+                    </div>
+                </div>
+                <div className="mainBox">
+                    <div className="card">
+                        김관자
+                        <h1>김첨지가 왜 그랬을까요?</h1>
+                        <img src={MAIN_IMG} alt="" />
+                    </div>
+                </div>
+                <div className="mainBox">
+                    <div className="card">
+                        김관자
+                        <h1>김첨지가 왜 그랬을까요?</h1>
+                        <img src={MAIN_IMG} alt="" />
+                    </div>
+                </div>
+                <div className="mainBox">
+                    <div className="card">
+                        김관자
+                        <h1>김첨지가 왜 그랬을까요?</h1>
+                        <img src={MAIN_IMG} alt="" />
+                    </div>
+                </div>
+                <div className="mainBox">
+                    <div className="card">
+                        김관자
+                        <h1>김첨지가 왜 그랬을까요?</h1>
+                        <img src={MAIN_IMG} alt="" />
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
