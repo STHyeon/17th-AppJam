ad7df2c885594a46e97b4aeac8ccb6272c0a57b6
STHyeon <toby2009@naver.com>
2020-01-31 11:57:27 +0900

    INIT

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,17 +6,26 @@ import { App, AuthBox, ClockBox, FormBox } from "./containers";
 import { Redirect, Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import CalendarBox from "./containers/CalendarBox";
 
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+    uri: "http://localhost:8000/graphql"
+});
+
 ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route path="/" exact component={App} />
-            <Route path="/login" component={AuthBox} />
-            <Route path="/register" component={AuthBox} />
-            <Route path="/clock" component={ClockBox} />
-            <Route path="/calendar" component={CalendarBox} />
-            <Route path="/write" component={FormBox} />
-            <Redirect path="*" to="/" />
-        </Switch>
-    </Router>,
+    <ApolloProvider client={client}>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={App} />
+                <Route path="/login" component={AuthBox} />
+                <Route path="/register" component={AuthBox} />
+                <Route path="/clock" component={ClockBox} />
+                <Route path="/calendar" component={CalendarBox} />
+                <Route path="/write" component={FormBox} />
+                <Redirect path="*" to="/" />
+            </Switch>
+        </Router>
+    </ApolloProvider>,
     document.getElementById("root")
 );
